82d9507294c47d0fe6cedf35ba622821ac53d74f
Wes <hermes@ubuntu.(none)>
2013-03-27 00:43:00 -0400

    Generated hypotheses for A*

--- hw3/scientist.py
+++ hw3/scientist.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 #scientist.py
 #Weston Feely & Serena Jeblee
-#3/22/13
-import sys
+#3/27/13
+import sys, random
 
 class TM(object):
 	#Read translation model from file and organize into dictionaries
@@ -28,7 +28,7 @@ class TM(object):
 				subset[ngram] = self.spanish_ngrams[ngram]
 		return subset
 
-'''Returns ngrams of given order for a sentence, as a list of words'''
+#Returns ngrams of given order for a sentence, as a list of words
 def get_ngrams(sent,n):
 	if (len(sent) < n) or (n < 1): return []
 	ngrams = []
@@ -40,22 +40,45 @@ def get_ngrams(sent,n):
 		ngrams.append(buff.rstrip())
 	return ngrams
 
+#Make a random translation hypothesis for a given sentence's unigram list
 def make_hypothesis(unigrams,searchspace):
 	hyp = ''
 	score = 0.0
-	return hyp,score
+	i=0
+	#Loop through unigrams
+	while i < len(unigrams):
+		#Choose Spanish phrase of random length from beginning of unigrams
+		if i == (len(unigrams)-1):
+			phrase_size = 1
+		else:		
+			phrase_size = random.randint(1,len(unigrams)-i-1)
+		poss = ' '.join(unigrams[i:(i+phrase_size)])
+		#Make sure our possible Spanish phrase is in the searchspace
+		while poss not in searchspace:
+			phrase_size -=1
+			poss = ' '.join(unigrams[i:(i+phrase_size)])
+			if phrase_size < 1: break # unknown unigram
+		#Get a random translation for this Spanish phrase
+		if phrase_size >= 1:
+			lex_trans = searchspace[poss]
+			pair = lex_trans[random.randint(0,len(lex_trans)-1)]
+			hyp += pair[0]+' ' # add English string to hypothesis
+			score += pair[1] # add logprob to score
+			i += phrase_size
+		else:
+			i += 1
+	return hyp.strip(), score
 
-#def main(args):
-transfile = open('data/tm').readlines()
-data = open('data/input').readlines()
-tm = TM(transfile)
-	#for line in data:
-		#unigrams = line.strip().split()
-unigrams = data[3].strip().split()
-searchspace = tm.getsearchspace(unigrams)
-		#hyp,score = make_hypothesis(unigrams,searchspace)
-		#print str(score)+' '+hyp
-	#return 0
+def main(args):
+	transfile = open('data/tm').readlines()
+	data = open('data/input').readlines()
+	tm = TM(transfile)
+	for line in data:
+		unigrams = line.strip().split()
+		searchspace = tm.getsearchspace(unigrams)
+		hyp,score = make_hypothesis(unigrams,searchspace)
+		print str(score)+' '+hyp
+	return 0
 
-#if __name__ == '__main__':
-#	sys.exit(main(sys.argv))
+if __name__ == '__main__':
+	sys.exit(main(sys.argv))
