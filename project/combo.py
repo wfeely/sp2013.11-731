8ed1c4e8460bccc0377d2e6987a9d47b5de55b2b
Wes <hermes@ubuntu.(none)>
2013-04-23 18:09:06 -0400

    fixed suffix combiner

--- project/combo.py
+++ project/combo.py
@@ -12,8 +12,8 @@ def main(args):
 	if len(args) < 3:
 		print "Usage: python combo.py list1.txt list2.txt"
 		return 1
-	s1 = set(open(args[1]).readlines()) # read in first text file as a set
-	s2 = set(open(args[2]).readlines()) # read in second text file as a set
+	s1 = set([x.strip() for x in open(args[1]).readlines()]) # read in first text file as a set
+	s2 = set([x.strip() for x in open(args[2]).readlines()]) # read in second text file as a set
 	#Make an output set, as the union of both s1 and s2
 	out_set = s1 | s2
 	#Add cartesian product of s1 and s2 to out_set
