a600141c48e2b0202dae0254ca677d6cc4f89bff
Wes <hermes@ubuntu.(none)>
2013-04-29 02:20:43 -0400

    modified batch run-moses

--- project/batch_run_moses.sh
+++ project/batch_run_moses.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #batch_run_moses.sh
 #Weston Feely
-#4/28/13
+#4/29/13
 
 #echo "Running Moses testing on EGY..."
 #./run_moses.sh egy
@@ -10,15 +10,15 @@
 #echo "Running Moses testing on EGYLEV..."
 #./run_moses.sh egylev
 
-echo "Running Moses testing on EGY-MADA-NP..."
-./run_moses.sh egymadanp
-echo "Running Moses testing on LEV-MADA-NP..."
-./run_moses.sh levmadanp
-echo "Running Moses testing on EGYLEV-MADA-NP..."
-./run_moses.sh egylevmadanp
+#echo "Running Moses testing on EGY-MADA-NP..."
+#./run_moses.sh egymadanp
+#echo "Running Moses testing on LEV-MADA-NP..."
+#./run_moses.sh levmadanp
+#echo "Running Moses testing on EGYLEV-MADA-NP..."
+#./run_moses.sh egylevmadanp
 
-echo "Running Moses testing on EGY-MISH..."
-./run_moses.sh egymish
+#echo "Running Moses testing on EGY-MISH..."
+#./run_moses.sh egymish
 echo "Running Moses testing on LEV-MISH..."
 ./run_moses.sh levmish
 echo "Running Moses testing on EGYLEV-MISH..."
